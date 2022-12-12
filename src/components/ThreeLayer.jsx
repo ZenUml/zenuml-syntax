97043f9ef79a18520cac12da23f6601e3af80cd4
MrCoder <eagle.xiao@gmail.com>
2022-12-13 10:17:31 +1100

    Fixed the animation style (colors).

--- src/components/ThreeLayer.jsx
+++ src/components/ThreeLayer.jsx
@@ -29,12 +29,12 @@ export function ThreeLayer({ type = 'note', title, children }) {
       <div className={clsx('container my-8 flex justify-center rounded-3xl p-6', styles[type].container)}>
         <div className="flex justify-center ml-4 flex-auto pl-20">
           <div className="box relative bg-violet-600 h-64 w-64 flex flex-col text-center p-2 text-white">
-            <span className="title">DiagramFrame</span>
+            <span className="title text-black">DiagramFrame</span>
             <div className="relative flex-grow">
-              <div className="lifeline-layer h-full w-full flex justify-center absolute bg-green-700 border-dashed border-2 border-white">
+              <div className="lifeline-layer h-full w-full flex justify-center absolute bg-green-700">
                 <div className="content">LifelineLayer</div>
               </div>
-              <div className="message-layer h-full w-full flex justify-center absolute bg-sky-700 border-dashed border-2 border-white">
+              <div className="message-layer h-full w-full flex justify-center absolute bg-sky-700">
                 <div className="content">MessageLayer</div>
               </div>
             </div>
